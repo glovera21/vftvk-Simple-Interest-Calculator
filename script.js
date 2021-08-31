2b8007c4826f90460fd2a7f8859434670b5bc0c8
glovera21 <89586008+glovera21@users.noreply.github.com>
2021-08-31 14:06:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function compute()
 var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
 var interest= principal*years*rate /100;
+   
 var year = Date().getFullYear()+parseInt(years)
 }
  function updateRate() 
